e7d68667900ff16a0030f51679f976d1d264155a
Fedor <topsandstudio@gmail.com>
2022-07-13 20:46:28 +0300

    logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useTransition } from 'react'
+import A from "./app.module.css"
+import { data } from './data'
+import Item from './components/Item'
+
+const dataSort = {
+  name : 'A',
+  address: 'A',
+  phone:'A',
+  email: 'A'
+}
 
 function App() {
+  const [state, setState]=useState(data)
+  const [search,setSearch]=useState("")
+  const [isLoading, startTransition] = useTransition()
+
+  const sortName = (key) =>{  ///  'name' ; 'adress' ; 'email'  // a[email]
+
+    const newArr = state.sort((a, b)=>{
+
+      const nameA = a[key].toUpperCase();
+      const nameB = b[key].toUpperCase();
+
+      if(dataSort[key]==="A"){
+        if (nameA > nameB) {
+          return -1;
+        }
+        if (nameA < nameB) {
+          return 1;
+        }
+      }
+      else{
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+      }
+      return 0;
+    })
+    setState(newArr)
+  }
+
+  const transform = (n)=>{
+    const res = n.match(/\d+/g)
+    return +res.join("")
+  }
+
+  const sortPhone = (key)=>{
+
+    const newArr = state.sort(function (a, b) {
+
+       if(dataSort[key]==="A") {
+        return  transform(a.phone) - transform(b.phone)
+       } else {
+        return  transform(b.phone) - transform(a.phone)
+      }
+    });
+    setState(newArr)
+  }
+
+  const sort = (e) => startTransition (()=>{
+    const key = e.target.value
+  dataSort[key] = dataSort[key] == 'A' ? 'B' :'A'
+
+      switch (key){
+        case 'phone':sortPhone(key);
+          break
+        default :sortName(key);
+          break
+      }
+
+  })
+
+  const filteredArray = (text) => {
+    text = text.toLowerCase().trim()
+  const newArr =  data
+    .filter(elem =>
+    elem.name.toLowerCase().includes(text) ||
+    elem.address.toLowerCase().includes(text) ||
+    elem.email.toLowerCase().includes(text) ||
+    elem.phone.toLowerCase().includes(text)
+  )
+    setState(newArr)
+  }
+
+  const sortInput = (e)=>{
+     setSearch(e.target.value)
+
+     filteredArray(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={A.App}>
+
+      <input type="search"
+             placeholder='search'
+             onInput={sortInput}
+
+      />
+
+      <header className={A.C_beetween} onClick={sort}>
+        <button className={A.elem} value="name">Name</button>
+        <button className={A.elem + ' ' + A.elemAddress } value="address">Address</button>
+        <button className={A.elem} value="phone">Phone</button>
+        <button className={A.elem + ' ' + A.elemEmail} value="email">Email</button>
       </header>
+
+      <section className={A.info}>
+        {
+          isLoading
+            ? 'Loading...'
+            : state.map((elem)=><Item key={elem.guid} {...elem} />)
+        }
+
+      </section>
     </div>
   );
 }
 
 export default App;
+
+
+//
+//
+//
+// let n1 = '(589) 858-9752'
+// let n2 = '1-457-571-4196'
+//
+//
+// const transform = (n)=>{
+//   const res = n.match(/\d+/g)
+//   return +res.join("")
+// }
+// transform(n1)
+// transform(n2)
---
+++ src/components/Item.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import A from "../app.module.css"
+
+function Item (props) {
+  return (
+    <div className={A.C_beetween}>
+      <div className={A.elem}>{props.name}</div>
+      <div className={A.elem + ' ' + A.elemAddress}>{props.address}</div>
+      <div className={A.elem}>{props.phone}</div>
+      <div className={A.elem + ' ' + A.elemEmail}>{props.email}</div>
+    </div>
+  )
+}
+
+export default Item
